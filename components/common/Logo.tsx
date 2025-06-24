6b53a42917046b7fe92c681ff6fe6a076f789f92
Samuel Koigor F. Turay <96601470+Koigor97@users.noreply.github.com>
2025-06-24 17:26:07 +0200

    change icon from H to RiCustomerService2Fill icon

--- components/common/Logo.tsx
+++ components/common/Logo.tsx
@@ -1,3 +1,4 @@
+import {RiCustomerService2Fill} from "@remixicon/react";
 
 type LogoProps = {
     className?: string;
@@ -7,7 +8,9 @@ export default function Logo({className}: LogoProps) {
   return (
     <div className={`${className} flex items-center gap-2`}>
         <div className="w-8 h-8 bg-primary rounded-lg flex items-center justify-center">
-            <span className="text-primary-foreground font-bold text-sm font-montserrat">H</span>
+            <span className="text-primary-foreground font-bold text-sm font-montserrat">
+                <RiCustomerService2Fill />
+            </span>
         </div>
         <span className="text-xl font-bold font-montserrat">HelpDesk</span>
     </div>
