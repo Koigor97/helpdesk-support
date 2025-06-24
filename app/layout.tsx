a66ec1649c01686c157d6c096b8711522ca8f351
Samuel Koigor F. Turay <96601470+Koigor97@users.noreply.github.com>
2025-06-24 13:14:53 +0200

    comment meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    {/*<meta name="apple-mobile-web-app-title" content="MyWebSite" />*/}
       <body
         className={`${inter.className} ${montserrat.className} font-sans antialiased h-screen`}
       >
