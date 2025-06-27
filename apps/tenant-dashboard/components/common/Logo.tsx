8a779ffa29b0ad3cefa4dd04f87b440d2ec024e3
Samuel Koigor F. Turay <96601470+Koigor97@users.noreply.github.com>
2025-06-28 00:03:34 +0200

    modify logo with some params

--- apps/tenant-dashboard/components/common/Logo.tsx
+++ apps/tenant-dashboard/components/common/Logo.tsx
@@ -2,17 +2,26 @@ import {RiCustomerService2Fill} from "@remixicon/react";
 
 type LogoProps = {
     className?: string;
+    justLogoIcon?: boolean;
 };
 
-export default function Logo({className}: LogoProps) {
+export default function Logo({className, justLogoIcon = false}: LogoProps) {
   return (
-    <div className={`${className} flex items-center gap-2`}>
-        <div className="w-8 h-8 bg-primary rounded-lg flex items-center justify-center">
-            <span className="text-primary-foreground font-bold text-sm font-montserrat">
-                <RiCustomerService2Fill />
-            </span>
-        </div>
-        <span className="text-xl font-bold font-montserrat">HelpDesk</span>
-    </div>
+      <>
+      {justLogoIcon !== false ? (
+          <span className="text-primary-foreground font-bold text-sm font-montserrat">
+              <RiCustomerService2Fill />
+          </span>
+        ) : (
+            <div className={`${className} flex items-center gap-2`}>
+                <div className="w-8 h-8 bg-primary rounded-lg flex items-center justify-center">
+                    <span className="text-primary-foreground font-bold text-sm font-montserrat">
+                        <RiCustomerService2Fill />
+                    </span>
+                </div>
+                <span className="text-xl font-bold font-montserrat">HelpDesk</span>
+            </div>)
+      }
+      </>
   )
 }
