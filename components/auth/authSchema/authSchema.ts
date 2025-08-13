9648f72f75a57b57050e4639190b601dcfe45f07
koigor97 <turaysamuel101@yahoo.com>
2025-08-13 13:59:04 +0200

    move from ../schemas to ../auth/authSchema folder

--- components/auth/authSchema/authSchema.ts
+++ components/auth/authSchema/authSchema.ts
@@ -50,7 +50,26 @@ export const ResendOTPSchema = z.object({
       .toLowerCase()
 })
 
+export const SignupFormSchema = z
+    .object({
+      email: z
+          .string()
+          .min(1, { message: "Email is required." })
+          .email({ message: "Please enter a valid email address." })
+          .trim()
+          .toLowerCase(),
+      fullName: z.string().min(1, { message: "Full name is required." }).trim(),
+      password: z.string().min(8, { message: "Password must be at least 8 characters long." }).trim(),
+      confirmPassword: z.string().min(8, { message: "Password must be at least 8 characters long." }).trim(),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: "Passwords don't match",
+      path: ["confirmPassword"],
+    })
+
+
 export type LoginFormData = z.infer<typeof LoginFormSchema>;
 export type MagicLinkData = z.infer<typeof MagicLinkSchema>
 export type VerifyOTPData = z.infer<typeof VerifyOTPSchema>;
 export type ResendOTPData = z.infer<typeof ResendOTPSchema>;
+export type SignupFormData = z.infer<typeof SignupFormSchema>
