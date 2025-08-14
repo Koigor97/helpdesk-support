7d1f0c95512a4f4cafb8905cd21ff2e0fe2e954d
koigor97 <turaysamuel101@yahoo.com>
2025-08-14 08:38:28 +0200

    add a showTitle prop

--- components/common/logo.tsx
+++ components/common/logo.tsx
@@ -1,10 +1,11 @@
 import {RiCustomerService2Fill} from "@remixicon/react";
 
 type LogoProps = {
+    showLogoTitle?: boolean;
     className?: string;
 };
 
-export default function Logo({className}: LogoProps) {
+export default function Logo({className, showLogoTitle = true}: LogoProps) {
   return (
     <div className={`${className} flex items-center gap-2`}>
         <div className="w-8 h-8 bg-primary rounded-lg flex items-center justify-center">
@@ -12,7 +13,7 @@ export default function Logo({className}: LogoProps) {
                 <RiCustomerService2Fill />
             </span>
         </div>
-        <span className="text-xl font-bold font-montserrat">HelpDesk</span>
+        {showLogoTitle && <span className="text-xl font-bold font-montserrat">WetinHapin</span>}
     </div>
   )
 }
