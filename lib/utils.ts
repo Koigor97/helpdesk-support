b35818737a7dc6ce0b221533319884581b614003
koigor97 <turaysamuel101@yahoo.com>
2025-08-14 08:16:15 +0200

    add a utils file for helper functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,19 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
+/** Tailwind class merger (shadcn) */
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+/** Formats an ISO string as a readable date+time. */
+export function formatDateTime(iso: string): string {
+  const d = new Date(iso);
+  return new Intl.DateTimeFormat(undefined, {
+    year: "numeric",
+    month: "short",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+  }).format(d);
 }
