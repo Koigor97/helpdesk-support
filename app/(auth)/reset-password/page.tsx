a410123f1a003c112ca9d2c66c3f2661da8e18c5
koigor97 <turaysamuel101@yahoo.com>
2025-08-16 21:43:53 +0200

    create reset-password page

---
+++ app/(auth)/reset-password/page.tsx
@@ -0,0 +1,32 @@
+
+import {Suspense} from "react";
+import ResetPasswordForm from "@/components/auth/resetPasswordForm";
+import {Loader2} from "lucide-react";
+
+function LoadingFallback() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-background p-4">
+            <div className="w-full max-w-md flex items-center">
+                <Loader2 className="w-6 h-6 animate-spin" />
+            </div>
+        </div>
+    )
+}
+
+export default function ResetPasswordPage() {
+    return (
+        <div className="min-h-screen flex flex-col items-center bg-background p-4">
+            <div className="w-full max-w-m mb-14">
+                <div className="text-center flex flex-col gap-2">
+                    <h1 className="h2-semibold">Set new password</h1>
+                    <p>Enter your new password below to complete the reset process.</p>
+                </div>
+            </div>
+            <div>
+                <Suspense fallback={<LoadingFallback />}>
+                    <ResetPasswordForm />
+                </Suspense>
+            </div>
+        </div>
+    )
+}
