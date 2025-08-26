c797909132175da34a3a13f8706ada243f73a476
koigor97 <turaysamuel101@yahoo.com>
2025-08-26 19:19:10 +0200

    modify, refactor middleware file for domain-based mapping

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
-import {NextResponse, NextRequest} from "next/server";
-import {middlewareClient} from "@/lib/supabase-clients/middlewareClient";
+import { NextResponse, NextRequest } from "next/server";
 
+import { middlewareClient } from "@/lib/supabase-clients/middlewareClient";
+import {buildUrl, getHostnameAndPort} from "@/lib/globalHelpers";
+import {sbAdminClient} from "@/lib/supabase-clients/adminClient";
 
 export const config = {
     matcher: ["/((?!.*\\.).*)"]
@@ -8,28 +10,58 @@ export const config = {
 
 
 export async function middleware(req: NextRequest) {
-
     const { supabase, response } = await middlewareClient(req);
-    const {data: {session: sessionUser}} = await supabase.auth.getSession()
+    const {data: sessionUser} = await supabase.auth.getUser()
+
+    const [hostname] = getHostnameAndPort(req)
+
+    const superbaseAdmin  = sbAdminClient()
+
+    const { data: tenantData, error: tenantError} = await superbaseAdmin
+        .from("tenants")
+        .select("*")
+        .eq("domain", hostname)
+        .single()
+
+    if (tenantError) {
+        return NextResponse.rewrite(new URL("/not-found", req.url));
+    }
+
     const requestedPath = req.nextUrl.pathname;
 
-    const [tenant, ...restOfUrlPath] = requestedPath.substring(1).split("/")
-    const appPath  = "/" + restOfUrlPath.join("/");
+    const tenant = tenantData.id;
+    const appPath  = requestedPath;
 
     if ( !/[a-z0-9-_]+/.test(tenant)){
         return NextResponse.rewrite(new URL(`/not-found?tenant=${tenant}`, req.url));
     }
 
     if (appPath.startsWith("/tickets")){
-        if(!sessionUser?.user){
-            return NextResponse.redirect(new URL(`/${tenant}`, req.url));
+        if (!sessionUser?.user){
+            return NextResponse.redirect(buildUrl("/", tenant, req));
+
+        } else if (!sessionUser?.user.app_metadata?.tenants.includes(tenant)){
+            return NextResponse.rewrite(new URL(`/not-found?tenant=${tenant}`, req.url));
         }
+
     } else if (appPath === "/") {
-        if(sessionUser?.user) {
-            return NextResponse.redirect(new URL(`/${tenant}/tickets`, req.url));
+        if (sessionUser?.user) {
+            return NextResponse.redirect(buildUrl("/tickets", tenant, req));
         }
     }
 
-    return response.value;
+    console.log(req.url)
+
+    const rewrittenResponse = NextResponse.rewrite(
+        new URL(`/${tenant}${appPath}${req.nextUrl.search}`, req.url),
+        { request: req }
+    );
+
+    const cookiesToSet = response.value.cookies.getAll();
+    cookiesToSet.forEach(({ name, value }) => {
+        rewrittenResponse.cookies.set(name, value, options)
+    })
+
+    return rewrittenResponse;
 
 }
